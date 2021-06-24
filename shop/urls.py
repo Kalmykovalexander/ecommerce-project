f43c71d3fbeb500041645f00685a8c61f31d6e30
Alexander <kalmykovalexander28@gmail.com>
2021-06-24 15:18:28 +0300

    Add product page view from home page

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,5 @@ from shop import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('<slug:category_slug>', views.home, name='products_by_name'),
-    path('product/', views.product, name='product'),
+    path('<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,11 +5,16 @@ def home(request, category_slug=None):
     category_page = None
     products = None
     if category_slug != None:
-        category_page = get_object_or_404(Category, slug=category_slug)
+        category_page = get_object_or_404(Category, category=category_slug)
         products = Product.objects.filter(category=category_page, available=True)
     else:
         products = Product.objects.all().filter(available=True)
     return render(request, 'home.html', {'category': category_page, 'products': products})
 
-def product(request):
-    return render(request, 'product.html')
+
+def product(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
