3f6cf84e2962ebb888a07065b1faa79595340d2f
Alexander <kalmykovalexander28@gmail.com>
2021-06-24 16:57:16 +0300

    Add cart page

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_name'),
-    path('<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('category/<slug:category_slug>', views.home, name='products_by_name'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('cart/', views.cart, name='cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
+
 def home(request, category_slug=None):
     category_page = None
     products = None
     if category_slug != None:
-        category_page = get_object_or_404(Category, category=category_slug)
+        category_page = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category_page, available=True)
     else:
         products = Product.objects.all().filter(available=True)
@@ -18,3 +19,7 @@ def product(request, category_slug, product_slug):
     except Exception as e:
         raise e
     return render(request, 'product.html', {'product': product})
+
+
+def cart(request):
+    return render(request, 'cart.html')
